9277bdf93c1e6030be056f70545462bbf8c255b6
kennedyowusu <kennediowusu@gmail.com>
2023-02-23 13:23:40 +0000

    insert data into owners and species table

--- data.sql
+++ data.sql
@@ -35,3 +35,16 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, 
 ('Ditto', '2022-05-14', 4, true, 22, 'Unknown');
 
 -- DAY TWO TASK ENDS HERE
+
+-- DAY THREE TASK STARTS HERE
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon'), ('Digimon');
